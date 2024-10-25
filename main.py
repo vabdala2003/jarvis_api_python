eb45a44b679d892290f0e0b0e8e9f49d448ef742
vabdala2003 <viniciusmta2003@gmail.com>
2024-10-25 20:46:15 -0300

    API finalizada

---
+++ main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+from controller.routers import jarvis_operacoes
+from shared.database import Base, engine
+from shared.database import *
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(jarvis_operacoes.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+# http://localhost:8000/
+# http://localhost:8000/docs
