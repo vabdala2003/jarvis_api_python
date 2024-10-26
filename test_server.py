185db3c719928f8b106335f2e0f7f4e800f73af3
vabdala2003 <viniciusmta2003@gmail.com>
2024-10-25 21:07:46 -0300

    Update

--- test_server.py
+++ test_server.py
@@ -1,4 +1,23 @@
 import requests
 
-print(requests.get("http://127.0.0.1:8000/items/110").json())
-print(requests.get("http://127.0.0.1:8000/items?name=Nails").json())
+BASE_URL = "http://127.0.0.1:8000/jarvis"
+
+def test_listar_produtos():
+    response = requests.get(f"{BASE_URL}/produtos")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)  # Verifica se a resposta é uma lista de produtos
+
+def test_cadastrar_produto():
+    novo_produto = {
+        "nm_produto": "Produto Teste",
+        "ds_categoria": "Categoria Teste",
+        "nr_preco": 99.99,
+        "st_produto": "D",
+        "ds_produto": "Descrição do Produto Teste",
+        "nr_tamanho": "M"
+    }
+    response = requests.post(f"{BASE_URL}/produtos", json=novo_produto)
+    assert response.status_code == 201
+    data = response.json()
+    assert data["nm_produto"] == novo_produto["nm_produto"]
+    assert data["nr_preco"] == novo_produto["nr_preco"]
